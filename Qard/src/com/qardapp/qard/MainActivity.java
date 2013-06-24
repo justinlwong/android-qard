5de6075bbdcabcc78fccd42d441366e3926ccf3c
Raymond Lam <ray@Raymonds-MacBook-Pro.local>
2013-06-24 14:01:07 -0400

    Removed Viewpager and Pageradapter code.

--- Qard/src/com/qardapp/qard/MainActivity.java
+++ Qard/src/com/qardapp/qard/MainActivity.java
@@ -45,23 +45,10 @@ import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends SherlockFragmentActivity implements
-		ActionBar.TabListener, LoaderCallbacks<ArrayList<ServerNotifications>> {
+public class MainActivity extends SherlockFragmentActivity implements LoaderCallbacks<ArrayList<ServerNotifications>> {
 
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	//SectionsPagerAdapter mSectionsPagerAdapter;
 	public String qrcode = "";
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	//ViewPager mViewPager;
+
 	public static int FRAG_PROFILE = 0;
 	public static int FRAG_FRIENDS = 1;
 	public static int FRAG_SETTINGS = 2;
@@ -86,9 +73,9 @@ public class MainActivity extends SherlockFragmentActivity implements
 
 		// Consider using menu items
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-		  actionBar.setDisplayHomeAsUpEnabled(false);
-		    actionBar.setHomeButtonEnabled(true);
-		    actionBar.setDisplayUseLogoEnabled(false);
+		actionBar.setDisplayHomeAsUpEnabled(false);
+		actionBar.setHomeButtonEnabled(true);
+		actionBar.setDisplayUseLogoEnabled(false);
 		actionBar.setDisplayShowTitleEnabled(false);
 		actionBar.setDisplayShowHomeEnabled(true);
 
@@ -98,38 +85,6 @@ public class MainActivity extends SherlockFragmentActivity implements
 		// !! TEST: GET FACEBOOK USER INFO
 		//FacebookConnect fc = new FacebookConnect(this);
 		//fc.getUserInfo();
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the app.
-		/*mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
-
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);*/
-		
-
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-	/*	mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						actionBar.setSelectedNavigationItem(position);
-					}
-				});
-*/
-		/*
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by
-			// the adapter. Also specify this Activity object, which implements
-			// the TabListener interface, as the callback (listener) for when
-			// this tab is selected.
-			actionBar.addTab(actionBar.newTab()
-					.setText(mSectionsPagerAdapter.getPageTitle(i))
-					.setTabListener(this));
-		}*/
 	
 		ImageView menu_friends = (ImageView) v.findViewById(R.id.menu_friends);
 		menu_friends.setOnClickListener(new OnClickListener() {
@@ -179,7 +134,7 @@ public class MainActivity extends SherlockFragmentActivity implements
 			trans.replace(R.id.main_container, fragment, "FRAG_FRIENDS");
 			trans.addToBackStack(null);
 			trans.commit();
-			// Close search box
+			// Close keyboard
 			FrameLayout layout = (FrameLayout) MainActivity.this
 					.findViewById(R.id.main_container);
 			layout.requestFocus();
@@ -230,25 +185,7 @@ public class MainActivity extends SherlockFragmentActivity implements
 		}
 		return true;
 	}
-	
-	@Override
-	public void onTabSelected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-		// When the given tab is selected, switch to the corresponding page in
-		// the ViewPager.
-		//mViewPager.setCurrentItem(tab.getPosition());
-	}
 
-	@Override
-	public void onTabUnselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	@Override
-	public void onTabReselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data)
 	{  
@@ -258,59 +195,6 @@ public class MainActivity extends SherlockFragmentActivity implements
 		}
 	}
 
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Log.d("Frag", "" +position);
-			if (position == 0) {
-				Fragment fragment = new ProfileFragment();
-				return fragment;
-			}
-			else if (position == 1) {
-				Fragment fragment = new FriendsFragment();
-				return fragment;
-			}
-			else if (position == 2) {
-				Fragment fragment = new SettingsFragment();
-				return fragment;
-			}
-			else{
-				return null;
-			}
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_profile).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_friends).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_settings).toUpperCase(l);
-			}
-			return null;
-		}
-	}
 
 	@Override
 	public Loader<ArrayList<ServerNotifications>> onCreateLoader(int id,
