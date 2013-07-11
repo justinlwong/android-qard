51b0cbc6545ce73488caf223ae78b8cd6bdbb3b0
Gavin <gavin20151@gmail.com>
2013-07-11 03:41:42 -0400

    widget opens new activity by clicking camera icon

--- Qard/src/com/qardapp/qard/widget/QardWidgetIntentReceiver.java
+++ Qard/src/com/qardapp/qard/widget/QardWidgetIntentReceiver.java
@@ -1,10 +1,14 @@
 package com.qardapp.qard.widget;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.qardapp.qard.MainActivity;
 import com.qardapp.qard.R;
 
 
@@ -15,11 +19,18 @@ public class QardWidgetIntentReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		if(intent.getAction().equals("pl.looksok.intent.action.CHANGE_PICTURE")){
-			updateWidgetPictureAndButtonListener(context);
+			updateWidgetPictureAndButtonListener(context, "CHANGE_PICTURE");
 		}
+		/*
+		if(intent.getAction().equals("pl.looksok.intent.action.CAMERA")){
+			cameraWidgetButtonListener(context, "CAMERA");
+		}
+		*/
+
+		
 	}
 
-	private void updateWidgetPictureAndButtonListener(Context context) {
+	private void updateWidgetPictureAndButtonListener(Context context, String widgetAction) {
 		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
 		remoteViews.setImageViewResource(R.id.widget_image, getImageToSet());
 
@@ -28,9 +39,36 @@ public class QardWidgetIntentReceiver extends BroadcastReceiver {
 
 		QardWidgetProvider.pushWidgetUpdate(context.getApplicationContext(), remoteViews);
 	}
+	
+	/*
+	private void cameraWidgetButtonListener(Context context, String widgetAction){
+		//Log.v("toggle_widget","Enabled is being called"); 
+
+        AppWidgetManager mgr = AppWidgetManager.getInstance(context); 
+        //retrieve a ref to the manager so we can pass a view update 
+
+        Intent i = new Intent(); 
+        i.setClassName("com.qardapp.qard.widget", "com.qardapp.qard.MainActivity"); 
+        PendingIntent myPI = PendingIntent.getService(context, 0, i, 0); 
+        //intent to start service 
+
+      // Get the layout for the App Widget 
+      RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_main); 
+
+      //attach the click listener for the service start command intent 
+      views.setOnClickPendingIntent(R.id.menu_camera, myPI); 
+
+      //define the componenet for self 
+      ComponentName comp = new ComponentName(context.getPackageName(), QardWidgetIntentReceiver.class.getName()); 
+
+      //tell the manager to update all instances of the toggle widget with the click listener 
+      mgr.updateAppWidget(comp, views); 
+	}
+	*/
+	
 
 	private int getImageToSet() {
 		clickCount++;
-		return clickCount % 2 == 0 ? R.drawable.service_twitter : R.drawable.service_facebook;
+		return clickCount % 2 == 0 ? R.drawable.nexcircle : R.drawable.service_facebook;
 	}
 }
--- Qard/src/com/qardapp/qard/widget/QardWidgetProvider.java
+++ Qard/src/com/qardapp/qard/widget/QardWidgetProvider.java
@@ -1,25 +1,39 @@
 package com.qardapp.qard.widget;
 
-import com.qardapp.qard.R;
-
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
+
+import com.qardapp.qard.MainActivity;
+import com.qardapp.qard.R;
+import com.qardapp.qard.qrcode.QRCodeManager;
 
 public class QardWidgetProvider extends AppWidgetProvider {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
-
 		RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+		
+		//change images
 		remoteViews.setOnClickPendingIntent(R.id.widget_button, buildButtonPendingIntent(context));
-
 		pushWidgetUpdate(context, remoteViews);
+		//
+		
+		//Camera
+	    Intent configIntent = new Intent(context, MainActivity.class);
+	    //QRCodeManager.scanQRCode(configIntent);
+	    PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+	    remoteViews.setOnClickPendingIntent(R.id.widget_camera, configPendingIntent);
+	    appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
 	}
 
 	public static PendingIntent buildButtonPendingIntent(Context context) {
