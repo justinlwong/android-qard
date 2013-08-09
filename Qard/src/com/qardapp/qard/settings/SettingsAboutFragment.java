309abfb478a18afbec596943e5acf44bec6bba27
Gavin <gavin20151@gmail.com>
2013-08-08 20:12:52 -0400

    about page button implemented

--- Qard/src/com/qardapp/qard/settings/SettingsAboutFragment.java
+++ Qard/src/com/qardapp/qard/settings/SettingsAboutFragment.java
@@ -1,6 +1,7 @@
 package com.qardapp.qard.settings;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +27,9 @@ public class SettingsAboutFragment extends BaseFragment{
 			public void onClick(View v) {
 				//Intent intent = new Intent(getActivity(),QardLoginActivity.class);
 				//startActivityForResult(intent, 21);
+				Uri uri = Uri.parse("http://www.nexcircle.com");
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				startActivity(intent);
 			}
 		});
 		return rootView;
